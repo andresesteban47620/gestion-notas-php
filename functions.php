9fcaf5ac437a1eead304a73b97ed3e79587008cf
andresesteban47620 <andresesteban47620@gmail.com>
2026-02-11 19:15:17 -0500

    Delete functions.php

--- functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-function resultadoCualitativo(float $promedio): string {
-  if ($promedio < 5) return "Suspenso";
-  if ($promedio < 7) return "Bien";
-  if ($promedio < 9) return "Notable";
-  return "Sobresaliente";
-}
-
-function h($s) { return htmlspecialchars($s ?? "", ENT_QUOTES, "UTF-8"); }
