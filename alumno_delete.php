e5912a211b91143a6fbf162954c7f1cc2785f94c
andresesteban47620 <andresesteban47620@gmail.com>
2026-02-11 19:12:54 -0500

    Delete alumno_delete.php

--- alumno_delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . "/../config/db.php";
-
-$id = (int)($_GET["id"] ?? 0);
-$del = $pdo->prepare("DELETE FROM alumno WHERE id=?");
-$del->execute([$id]);
-
-header("Location: index.php");
-exit;
